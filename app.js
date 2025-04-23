0df8547474b77dce24ea0095e1b4ebeee3c82eee
Hnin Ei Shwe Yee <eshweyee3110@gmail.com>
2025-04-23 18:22:38 +0700

    created login logout

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express=require("express");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("./utils/database");
@@ -13,6 +15,17 @@ app.use("/img", express.static("public/img"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+const authMiddleware = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect("/");
+    }
+    next();
+};
+
+app.use("/",(req,res)=>{
+    res.render("logIn", { title: "Login for admin",errorMessage:""});
+});
 
 app.get("/product", (req, res) => {
     res.render("product", { title: "All Products"});
